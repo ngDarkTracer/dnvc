6d67bf8c84ea6fcde31f6c42c2c011bdb326dc3a
llorel <lorelkouam@gmail.com>
2021-10-04 13:00:12 +0200

    12H00

--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -124,7 +124,7 @@ export class MarketComponent implements OnInit {
                   title: elt.Title,
                   text: elt.Resume,
                   sourceType: elt.SourceFile.length === 0 ? 'url' : 'document',
-                  source: elt.SourceFile.length === 0 ? elt.sourceUrl : elt.sourceFile,
+                  source: elt.SourceFile.length === 0 ? elt.sourceUrl : this.serverAdress + elt.SourceFile[0].url,
                   markets: elt.Marches
                 }
               );
