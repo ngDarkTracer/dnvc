7273dee5792926840716b11e2880c9e04bcc7d7a
llorel <lorelkouam@gmail.com>
2021-10-21 10:58:23 +0200

    Version number 1.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,10 @@ import {TranslateService} from '@ngx-translate/core';
 })
 export class AppComponent {
   title = 'dnvc';
+  version = '1.0.0';
 
   constructor(private translate: TranslateService) {
     translate.setDefaultLang('fr');
+    console.log('DNVC version ' + this.version);
   }
 }
